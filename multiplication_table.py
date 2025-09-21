cddb0392d88c85289b5b1eb76021c5e2108a973b
JamieLaurels <03lucas.jude@kamsc.org>
2025-09-21 15:53:16 -0400

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
 def multiplication_table(n: int) -> None:
     for i in range(n):
         for j in range(n):
-            print(f"{i+1} * {j+1} = {(i+1)*(j+1)}")
+            print(f"{i+1} x {j+1} = {(i+1)*(j+1)}")
